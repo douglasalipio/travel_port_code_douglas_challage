5453ef7e340fa40fffc0a3fe0d6140ffdad6545a
Douglas Alipio <douglasalipio@gmail.com>
2020-02-12 10:35:24 +0000

    Testing mappers

--- app/src/test/java/com/baseproject/douglas/ForecastDtoMapperTest.kt
+++ app/src/test/java/com/baseproject/douglas/ForecastDtoMapperTest.kt
@@ -1,25 +1,43 @@
 package com.baseproject.douglas
 
+import com.baseproject.douglas.data.feature.weather.Main
+import com.baseproject.douglas.data.feature.weather.Sys
 import com.baseproject.douglas.data.feature.weather.WeatherDto
 import com.baseproject.douglas.data.feature.weather.WeatherDtoMapper
-import org.hamcrest.CoreMatchers.hasItem
-import org.junit.Assert.assertThat
+import com.baseproject.douglas.feature.weather.data.WeatherInfo
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-//class WeatherDtoMapperTest {
-//
-//    private val mapper = WeatherDtoMapper()
-//
-//    @Test
-//    fun `should map section model to section`() {
-//        // given
-//        val productDto = mockProductDto()
-//        val expectedProduct = mockProducts()
-//        // when
-//        val mappingResult = mapper.map(productDto)
-//        // then
-//        assertThat(mappingResult.categories, hasItem((expectedProduct.categories.last())))
-//    }
-//
-//    private fun mockWeatherDto() = WeatherDto()
-//}
+class WeatherDtoMapperTest {
+
+    private val mapper = WeatherDtoMapper()
+
+    @Test
+    fun `should validate weather mapper`() {
+        // given
+        val weatherDto = mockWeatherDto()
+        val expectedWeatherInfo = mockWeatherInfo()
+        // when
+        val mappingResult = mapper.map(weatherDto)
+        // then
+        assertEquals(expectedWeatherInfo.city, mappingResult.city)
+    }
+
+    private fun mockWeatherInfo() = WeatherInfo(
+        "city",
+        "country",
+        "40",
+        "20",
+        "25",
+        mutableListOf()
+    )
+
+    private fun mockWeatherDto() =
+        WeatherDto(
+            "city",
+            listOf(),
+            Main(1f, 2f, 2),
+            "11/11/2019",
+            Sys("country")
+        )
+}
--- app/src/test/java/com/baseproject/douglas/WeatherDtoMapperTest.kt
+++ app/src/test/java/com/baseproject/douglas/WeatherDtoMapperTest.kt
@@ -1,23 +1,44 @@
-//package com.baseproject.douglas
-//
-//import com.baseproject.douglas.data.feature.weather.WeatherDtoMapper
-//import org.hamcrest.CoreMatchers.hasItem
-//import org.junit.Assert.assertThat
-//import org.junit.Test
-//
-//class WeatherDetailMapperTest {
-//
-//    private val mapper = WeatherDtoMapper()
-//
-//    @Test
-//    fun `should map section model to section`() {
-//        // given
-//        val productDetailDto = listOf(mockProductDetailDto())
-//        val expectedProductDetail = mockProductDetail()
-//        // when
-//        val mappingResult = mapper.map(productDetailDto)
-//        // then
-//        assertThat(mappingResult, hasItem((expectedProductDetail)))
-//    }
-//
-//}
+package com.baseproject.douglas
+
+import com.baseproject.douglas.data.feature.forecast.ForecastDto
+import com.baseproject.douglas.data.feature.forecast.ForecastDtoMapper
+import com.baseproject.douglas.data.feature.forecast.Main
+import com.baseproject.douglas.data.feature.forecast.Weather
+import com.baseproject.douglas.feature.weather.data.ForecastInfo
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ForecastDtoMapperTest {
+
+    private val mapper = ForecastDtoMapper()
+
+    @Test
+    fun `should validate forecast mapper`() {
+        // given
+        val forecastDto = mockForecastDto()
+        val expectedForecastInfo = mockForecastInfo()
+        // when
+        val mappingResult = mapper.mapToList(forecastDto)
+        // then
+        assertEquals(expectedForecastInfo.iconUrl, mappingResult.last().iconUrl)
+    }
+
+    private fun mockForecastDto() = ForecastDto(
+        listOf(
+            com.baseproject.douglas.data.feature.forecast.List(
+                10L,
+                "2020-02-15 21:00:00",
+                Main(10f, 15f),
+                listOf(Weather("moderate rain", "iconUrl"))
+            )
+        )
+    )
+
+    private fun mockForecastInfo() = ForecastInfo(
+        "http://openweathermap.org/img/wn/iconUrl@2x.png",
+        "10",
+        "15",
+        "2020-02-15 21:00:00",
+        "moderate rain"
+    )
+}
